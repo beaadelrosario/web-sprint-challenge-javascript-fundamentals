170d365f40b72c579b7434e50b3a8c5f1adbfad2
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-06-19 09:27:35 -0700

    why is my this.length working?

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -13,7 +13,6 @@
   Formula for cuboid volume: length * width * height
 */
 
-
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
 
@@ -25,6 +24,19 @@
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+
+
+function CuboidMaker(attributes){
+  this.length = attributes.length
+  this.width = attributes.width;
+  this.height = attributes.height;
+};
+
+CuboidMaker.prototype.volume = function(){
+  return (${this.length} * ${this.width} * ${this.height});
+}
+
+
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
